032781b46496349be617280ec49402ce7a1522b3
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2015-04-16 15:29:25 +0300

    version 1.0.3-SNAPSHOT, updated esper version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject kholodilov/clj-esper "1.0.2" 
+(defproject kholodilov/clj-esper "1.0.3-SNAPSHOT" 
   :min-lein-version "2.0.0"
   :repositories [["codehaus" "http://repository.codehaus.org"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.espertech/esper "5.0.0" :exclusions [log4j]]]
+                 [com.espertech/esper "5.2.0" :exclusions [log4j]]]
   :description "Simple Esper wrapper for Clojure")
